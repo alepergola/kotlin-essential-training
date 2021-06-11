b69c4085c764f91d835dd583599948f559792696
Alessandro Pergola <ale.pergola@outlook.it>
2021-06-12 00:07:09 +0200

    Created test class

---
+++ src/test/kotlin/challenges/chapter3/Test.kt
@@ -0,0 +1,13 @@
+package challenges.chapter3
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Test {
+
+    @org.junit.jupiter.api.Test
+    fun test() {
+        assertEquals("", main(40))
+    }
+
+
+}
