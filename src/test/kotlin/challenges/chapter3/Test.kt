f6ee9541076e0226898ce3d4985e3fd71831f984
Alessandro Pergola <ale.pergola@outlook.it>
2021-06-12 00:31:41 +0200

    Added tests

--- src/test/kotlin/challenges/chapter3/Test.kt
+++ src/test/kotlin/challenges/chapter3/Test.kt
@@ -11,9 +11,26 @@ internal class Test {
         @JvmStatic
         fun getData(): List<Arguments> {
             return listOf(
+                Arguments.of(-10000, "You have -10000 point and are at the platinum level."),
+                Arguments.of(-5000, "You have -5000 point and are at the platinum level."),
+                Arguments.of(-1000, "You have -1000 point and are at the platinum level."),
                 Arguments.of(-1, "You have -1 point and are at the platinum level."),
                 Arguments.of(0, "You have 0 points and are at the pearl level."),
                 Arguments.of(1, "You have 1 point and are at the pearl level."),
+                Arguments.of(500, "You have 500 points and are at the pearl level."),
+                Arguments.of(999, "You have 999 points and are at the pearl level."),
+                Arguments.of(1000, "You have 1000 points and are at the silver level."),
+                Arguments.of(1001, "You have 1001 points and are at the silver level."),
+                Arguments.of(2500, "You have 2500 points and are at the silver level."),
+                Arguments.of(4999, "You have 4999 points and are at the silver level."),
+                Arguments.of(5000, "You have 5000 points and are at the gold level."),
+                Arguments.of(5001, "You have 5001 points and are at the gold level."),
+                Arguments.of(7500, "You have 7500 points and are at the gold level."),
+                Arguments.of(9999, "You have 9999 points and are at the gold level."),
+                Arguments.of(10000, "You have 10000 points and are at the platinum level."),
+                Arguments.of(10001, "You have 10001 points and are at the platinum level."),
+                Arguments.of(1_000_000, "You have 1000000 points and are at the platinum level."),
+                Arguments.of(999_999_999, "You have 999999999 points and are at the platinum level."),
             )
         }
     }
