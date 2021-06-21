def1126c40eedc9ba3f779bea02a76864b853b0e
Alessandro Pergola <ale.pergola@outlook.it>
2021-06-21 23:39:58 +0200

    Change to single line function

--- src/main/kotlin/challenges/chapter5/Chapter5Solution.kt
+++ src/main/kotlin/challenges/chapter5/Chapter5Solution.kt
@@ -2,10 +2,9 @@ package challenges.chapter5
 
 import java.io.File
 
-fun readDoubleListFromFile(filename: String): List<Double> {
-    return File(filename)
+fun readDoubleListFromFile(filename: String): List<Double> =
+    File(filename)
         .readLines()
         .mapNotNull { it.toDoubleOrNull() }
         .sorted()
         .toList()
-}
