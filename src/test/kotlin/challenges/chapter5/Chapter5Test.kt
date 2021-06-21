d6a506ea1ea69cbc6fd6da396d90a8e0e0d74866
Alessandro Pergola <ale.pergola@outlook.it>
2021-06-21 23:19:16 +0200

    Added test file

---
+++ src/test/kotlin/challenges/chapter5/Chapter5Test.kt
@@ -0,0 +1,28 @@
+package challenges.chapter5
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class Chapter5Test {
+
+    @Test
+    fun getPointsAndLevel_tests() {
+        assertEquals(EXPECTED_DOUBLES, readDoubleListFromFile(FILENAME))
+    }
+
+    companion object {
+        private val EXPECTED_DOUBLES = listOf(
+            10.10,
+            20.20,
+            30.30,
+            40.40,
+            50.50,
+            60.60,
+            70.70,
+            80.80,
+            90.90
+        )
+        private const val FILENAME = "resources/sales.txt";
+    }
+
+}
