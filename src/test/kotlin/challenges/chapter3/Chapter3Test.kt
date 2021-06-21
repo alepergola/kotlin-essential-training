3303b795f2c4569fc50022898c6508a96a88f336
Alessandro Pergola <ale.pergola@outlook.it>
2021-06-21 22:54:52 +0200

    Renamed files

--- src/test/kotlin/challenges/chapter3/Chapter3Test.kt
+++ src/test/kotlin/challenges/chapter3/Chapter3Test.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 
-internal class Test {
+internal class Chapter3Test {
 
     companion object {
         @JvmStatic
--- src/test/kotlin/challenges/chapter4/Chapter4Test.kt
+++ src/test/kotlin/challenges/chapter4/Chapter4Test.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import kotlin.test.assertEquals
 
-internal class Test {
+internal class Chapter4Test {
 
     companion object {
         @JvmStatic
